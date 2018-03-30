a83ee34898ef4d9aab86f80d0ba49bf7443b706c
 Ivanushka <ivanushka@Bomb.local>
2018-03-30 18:25:55 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # aws-lambda-pdf-generation
 Generate PDF files with custom content from HTML templates
+
+### Requirements
+- AWS account credentials
+
+### Install deps
+
+```bash
+pip install git+https://github.com/qoda/python-wkhtmltopdf.git
+pip install -r requirements.txt
+```
+
+### Deploy to Lambda
+
+```bash
+mkdir ~/.aws
+cat >> ~/.aws/config
+[default]
+aws_access_key_id=YOUR_ACCESS_KEY_HERE
+aws_secret_access_key=YOUR_SECRET_ACCESS_KEY
+region=YOUR_REGION (such as us-west-2, us-west-1, etc)
+
+chalice deploy
+```
---
+++ app.py
@@ -0,0 +1,35 @@
+import subprocess
+import boto3
+from chalice import Chalice
+from jinja2 import Template
+
+app = Chalice(app_name='html2pdf')
+s3 = boto3.client('s3', region_name='eu-central-1')
+BUCKET = 'bulyatestlambda'
+
+
+@app.route('/url2pdf', methods=['POST'])
+def url2pdf():
+    try:
+        request = app.current_request
+        ctx = request.json_body['template_context']
+        tpl = Template('<html>Hello {{ name }}!</html>')
+        html = tpl.render(ctx)
+        proc = subprocess.run(
+            ["./wkhtmltopdf2", "-", "-"],
+            input=bytes(html, encoding="utf-8"),
+            stdout=subprocess.PIPE,
+        )
+        pdf = str(proc.stdout)
+        key = 'test.pdf'
+
+        s3.put_object(
+            Bucket=BUCKET,
+            Key=key,
+            Body=pdf,
+        )
+        return {
+            "pdf_url": "https://{}.s3.amazonaws.com/{}".format(BUCKET, key)
+        }
+    except Exception as e:
+        return {'error': str(e)}
